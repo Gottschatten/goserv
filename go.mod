df083c9ff57bd61ff8f38b3def430ca5369f4d94
Marten Drews <mmd@taucherglocke.dev>
2024-09-05 13:19:08 +0200

    Add HashFunction

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/gottschatten/goserv
 
 go 1.22.3
 
-require github.com/a-h/templ v0.2.747 // indirect
+require (
+	github.com/a-h/templ v0.2.747 // indirect
+	golang.org/x/crypto v0.26.0 // indirect
+)
--- userLogic.go
+++ userLogic.go
@@ -2,9 +2,13 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
+	"golang.org/x/crypto/bcrypt"
 	"log"
 	"net/http"
 )
+
+var ErrNoUser = errors.New("User not found")
 
 type User struct {
 	Id       int    `json:"id"`
@@ -22,6 +26,7 @@ func (db *DB) postUser(w http.ResponseWriter, r *http.Request) {
 }
 
 func validateUser(w http.ResponseWriter, r *http.Request, db *DB) {
+	const cost = 15
 	decoder := json.NewDecoder(r.Body)
 	user := User{
 		Id:       db.id,
@@ -34,6 +39,15 @@ func validateUser(w http.ResponseWriter, r *http.Request, db *DB) {
 		respondWithError(w, http.StatusInternalServerError, "Error decoding user")
 		return
 	}
+
+	hashedPW, err := bcrypt.GenerateFromPassword([]byte(user.Password), cost)
+	if err != nil {
+		log.Printf("Error hashing password: %s", err)
+		respondWithError(w, http.StatusInternalServerError, "Error PW")
+		return
+	}
+	user.Password = string(hashedPW)
+
 	userR, err := db.CreateUser(user)
 	if err != nil {
 		log.Printf("Error Creating and Saving User: %s", err)
@@ -42,6 +56,27 @@ func validateUser(w http.ResponseWriter, r *http.Request, db *DB) {
 	}
 	respondWithJson(w, http.StatusCreated, userR)
 	return
+}
+
+func (db *DB) getUserByMail(email string) (User, error) {
+	dbs, err := db.loadDB()
+	if err != nil {
+		return User{}, err
+	}
+	for _, user := range dbs.Users {
+		if user.Email == email {
+			return user, nil
+		}
+	}
+	return User{}, ErrNoUser
+}
+
+func (db *DB) validateMail(email string) (bool, error) {
+	_, err := db.getUserByMail(email)
+	if err == ErrNoUser {
+		return true, nil
+	}
+	return false, err
 }
 
 // Copy Write Get Chirps Logic for user, problably add users/chirp in the other just load data and write the database/add to DBStructure before writing
